234a156fabeba28f6f8440d3c47a70d95f17a860
William Farley <williamfarley@Williams-MacBook-Pro.local>
2017-04-24 16:13:38 -0400

    Finished Tutorial

---
+++ LearnFirebase/ListItem.swift
@@ -0,0 +1,15 @@
+//
+//  ListItem.swift
+//  LearnFirebase
+//
+//  Created by William Farley on 4/24/17.
+//  Copyright © 2017 William Farley. All rights reserved.
+//
+
+import Foundation
+
+class ListItem {
+    var listItem = ""
+    var postedBy = ""
+    var listItemKey = ""
+}
